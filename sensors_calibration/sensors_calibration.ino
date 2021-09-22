16988d781e494e194e25692e381cdfa1eea317f1
RUKUNDO  Jean Claude <rukundocr@gmail.com>
2021-09-22 18:22:54 +0200

    Update sensors_calibration.ino
    adding dust sensor

--- sensors_calibration/sensors_calibration.ino
+++ sensors_calibration/sensors_calibration.ino
@@ -1,17 +1,34 @@
+/* ...............DUST SENSOR config.........*/
+int ledPower = 8;   //Connect 3 led driver pins of dust sensor to Arduino D2
+int measurePin = A1;
+int samplingTime = 280;
+int deltaTime = 40;
+int sleepTime = 9680;
+float voMeasured = 0;
+float calcVoltage = 0;
+float dustDensity = 0;
+/* ...............OUTPUT DEVICES CONFIG............*/
 int heating= 4;
 int cooling1=5;
 int cooling2 =6;
 int lighting =7;
+
+/* .........variable to cappture serial data from node-red...*/
 int incomingByte;      // a variable to read incoming serial data into
+/* ...............DHT11 configuration.............*/
 #include "DHT.h"
 #define DHTPIN 2    
 #define DHTPIN1 3  
 #define DHTTYPE DHT11   // DHT 22  (AM2302), AM2321
 DHT dht(DHTPIN, DHTTYPE);
 DHT dht1(DHTPIN1, DHTTYPE);
+
+/* ..............MQ135 ON PIN A0......*/
 int mq_135 =A0;
+
 void setup() {
   Serial.begin(9600);
+   pinMode(ledPower,OUTPUT);
   dht.begin();
   // initialize the LED pin as an output:
   pinMode(heating, OUTPUT);
@@ -84,7 +101,22 @@ void loop() {
   }
   //READING MQ-135
     int co2 = analogRead(mq_135);
-    
+  /* ...............DUST SENSOR READING ............*/
+digitalWrite(ledPower,LOW); // power on the LED
+  delayMicroseconds(samplingTime);
+  voMeasured = analogRead(measurePin); // read the dust value
+  delayMicroseconds(deltaTime);
+  digitalWrite(ledPower,HIGH); // turn the LED off
+  delayMicroseconds(sleepTime);
+  // 0 - 5V mapped to 0 - 1023 integer values
+  // recover voltage
+  calcVoltage = voMeasured * (5.0 / 1024.0);
+  // linear eqaution taken from http://www.howmuchsnow.com/arduino/airquality/
+  // Chris Nafis (c) 2012
+  dustDensity = 170 * calcVoltage - 0.1;
+
+  /* ...............sending data to serial and to  node-red ............*/
+ 
     // displaying data
  Serial.print("{\"Humidity1\":");
  Serial.print(h,3);
@@ -96,6 +128,8 @@ void loop() {
  Serial.print(t1);
  Serial.print(",\"CO2\":");
  Serial.print(co2);
+ Serial.print(",\"DUST\":");
+ Serial.print(dustDensity);
  Serial.println("}");
     
   /*
